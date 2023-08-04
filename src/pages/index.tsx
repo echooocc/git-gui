7d53c1b70f4f9e959a8c8abeaf460c1a450be44a
echooocc <echo2go@gmail.com>
2023-08-04 02:07:54 -0400

    update ui

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,23 @@
 import { Octokit } from "@octokit/core";
 import Image from "next/image";
-import { useEffect, useState } from "react";
+import { MouseEvent, useEffect, useState } from "react";
+import ReactMarkdown from "react-markdown";
+import remarkGfm from "remark-gfm";
+
+interface TreePath {
+  path: string;
+  sha: string;
+  url: string;
+}
 
 const octokit = new Octokit({
   auth: process.env.NEXT_PUBLIC_GIT_TOKEN,
 });
 export default function Home() {
   const [repo, setRepo] = useState("jobbler-chrome-extension");
+  const [treeData, setTreeData] = useState<TreePath[]>([]);
+  const [currentData, setCurrentData] = useState<TreePath>({});
+  const [previewData, setPreviewData] = useState<string>("");
 
   const loadRepoContent = async () => {
     const res = await octokit.request(
@@ -20,7 +31,6 @@ export default function Home() {
         },
       }
     );
-
     const {
       data: {
         commit: {
@@ -30,7 +40,6 @@ export default function Home() {
         },
       },
     } = res;
-
     const response = await octokit.request(
       "GET /repos/{owner}/{repo}/git/trees/{tree_sha}?recursive=true",
       {
@@ -42,25 +51,66 @@ export default function Home() {
         },
       }
     );
-
-    console.log("response", response);
+    const {
+      data: { tree },
+    } = response;
+    const treePaths = tree as TreePath[];
+    console.log("response", treePaths);
+    treePaths && setTreeData([...treePaths]);
   };
 
   const handleSubmit = async () => {
     await loadRepoContent();
   };
 
+  const openFile = async (tree: TreePath) => {
+    const res = tree.url && (await octokit.request(`GET ${tree.url}`));
+    const decodedContent = atob(res?.data?.content);
+    setPreviewData(decodedContent);
+    console.log("data content", res?.data?.content, decodedContent);
+  };
+
   return (
     <main
       className={`flex min-h-screen flex-col items-center justify-between p-24`}
     >
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
+      <div className="z-10 w-full max-w-5xl items-center font-mono text-sm lg:flex">
         <input
           type="text"
           placeholder="Input your repo name"
           onChange={(e) => setRepo(e.target.value)}
+          className="shadow appearance-none border rounded py-2 px-3 text-gray-700 leading-tight focus:outline-none focus:shadow-outline"
         />
-        <button onClick={handleSubmit}>Submit</button>
+        <button
+          onClick={handleSubmit}
+          className="bg-gray-500 hover:bg-gray-300 text-white font-bold py-2 px-4 rounded ml-10"
+        >
+          Submit
+        </button>
+      </div>
+      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm grid grid-cols-3 gap-2">
+        {!!treeData.length && (
+          <div className="text-xl mt-5 col-span-3">{repo} files:</div>
+        )}
+        <div className="mt-2 col-start-1 self-start">
+          {!!treeData.length &&
+            treeData.map((tree) => (
+              <div className="py-1" key={tree.sha}>
+                {tree.path}
+                <button
+                  onClick={() => openFile(tree)}
+                  className="ml-2 underline text-gray-500 hover:text-gray-400"
+                >
+                  preview
+                </button>
+              </div>
+            ))}
+        </div>
+        {!!previewData.length && (
+          <div className="col-span-2 self-start p-5 shadow-lg shadow-gray-500 md:shadow-xl overflow-hidden">
+            <ReactMarkdown children={previewData} remarkPlugins={[remarkGfm]} />
+          </div>
+        )}
       </div>
     </main>
   );
